bdfac12f309eab78fdf8c18e7c0c21a0bf85fda8
Tom Stephens <@u6953044@anu.edu.au>
2022-02-11 16:28:36 +1100

    Incorporated sheet creation into tracket.start()

--- Scripts/Sheet.py
+++ Scripts/Sheet.py
@@ -16,7 +16,7 @@ class Sheet:
         self.client = gspread.authorize(self.creds)
 
     # Create the Google sheet (only want to do if it doesn't already exist)
-    def create_tracker_sheet(self):
+    def create_tracker_sheet(self, advancement_list):
         ss = self.client.create(self.name)  # spreadsheet object
 
         with open("./Information/config.json", "r") as file:
@@ -27,7 +27,6 @@ class Sheet:
 
         # Populate First Tab
         worksheet = ss.get_worksheet(0)
-        advancement_list = []
         dict = {
             self.name: advancement_list}
         for player in range(self.players):
@@ -37,19 +36,6 @@ class Sheet:
         worksheet.update([df.columns.values.tolist()] + df.values.tolist())
         print(df)
 
-        # Add a tab for the 6 'progress' advancements
-        nether_prog_list = []
-        ss.add_worksheet("Explore Nether", nether_prog_list)
-        mobs_prog_list = []
-        ss.add_worksheet("Mobs", mobs_prog_list)
-        biomes_prog_list = []
-        ss.add_worksheet("Biomes", biomes_prog_list)
-        breed_animal_prog_list = []
-        ss.add_worksheet("Breed Animals", breed_animal_prog_list)
-        food_prog_list = []
-        ss.add_worksheet("Food", food_prog_list)
-        cats_prog_list = []
-        ss.add_worksheet("Cats", cats_prog_list)
 
     # Add a worksheet (tab) to the spreadsheet
     def add_worksheet(self, name, progress_list):
@@ -75,18 +61,6 @@ class Sheet:
         sheet = self.client.open(self.name).sheet1
         data = sheet.get_all_records()
         pprint(data)
-
-
-# new_sheet = Sheet("tester", players=3)
-
-# # new_sheet.display_advancements_tab()
-
-# # header = ['Explore Nether', 1, 2, 3]
-# # new_sheet.update_tracker_sheet(1, header)
-
-# new_sheet.create_tracker_sheet()
-# nether_list = ['biome 1', 'biome 2', 'biome 3', 'biome 4']
-# new_sheet.add_worksheet("Explore Nether 2", nether_list)
                         
 
 
